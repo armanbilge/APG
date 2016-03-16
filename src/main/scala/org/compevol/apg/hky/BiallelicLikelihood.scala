6e9604f49e7dc2215798269d241e3cdac9abaea0
Arman Bilge <armanbilge@gmail.com>
2016-03-17 08:35:00 +1300

    Overhaul design for HKY implementation

--- src/main/scala/org/compevol/apg/hky/BiallelicLikelihood.scala
+++ src/main/scala/org/compevol/apg/hky/BiallelicLikelihood.scala
@@ -4,31 +4,26 @@ import org.compevol.apg._
 
 import scala.collection.LinearSeq
 
-class BiallelicLikelihood(val taxa: Set[Taxon], val mu: Double, val kappa: Double, val pi: Map[Nucleotide, Double], val coalescentIntervals: LinearSeq[CoalescentInterval], val errorProbs: Taxon => Double) extends (Map[AlignmentColumn, Double] => Double) {
+class BiallelicLikelihood(val taxa: Set[Taxon], val mu: Double, val kappa: Double, val pi: IndexedSeq[Double], val coalescentIntervals: LinearSeq[CoalescentInterval], val errorProbs: Taxon => Double) extends (Map[Site, Double] => Double) {
 
-
-  override def apply(columns: Map[AlignmentColumn, Double]): Double = columns.view.par.map(Function.tupled({ (col, w) =>
+  override def apply(columns: Map[Site, Double]): Double = columns.view.par.map(Function.tupled({ (site, w) =>
 
     val samples = taxa.view.groupBy(_.height).map(Function.tupled({ (t, tx) =>
-      BiallelicSample(t, tx.view.map { t =>
-        col.sites(t) match {
-          case false => errorProbs(t)
-          case true => 1 - errorProbs(t)
-        }
-      }.toSeq)
+      BiallelicSample(t, site match {
+        case ConstantSite(_) => tx.view.map(errorProbs).toSeq
+        case TwoStateSite(_, _, f) => tx.view.map(t => if (f(t)) 1 - errorProbs(t) else errorProbs(t)).toSeq
+      })
     })).to[LinearSeq].sorted
 
-    val red = col.red
-    val L = col.green match {
-      case Some(green) =>
-        val piRed = Nucleotide.unambiguous(red).map(pi).sum
-        val piGreen = Nucleotide.unambiguous(green).map(pi).sum
-        (new BiallelicCoalescentLikelihood(mu, (piRed, piGreen), coalescentIntervals))(samples)
-      case None =>
-        0.0
-    }
+    // TODO
+//    val L = (site match {
+//      case ConstantSite(n) => (Nucleotide.stateSet - n).view.map((_, n))
+//      case TwoStateSite(red, green, _) => Traversable((red, green))
+//    }).map(Function.tupled { (red, green) =>
+//      def freq(n: Nucleotide) = Nucleotide.unambiguous(n).map()
+//    })
 
-    w * L
+    0.0
 
   })).sum
 
---
+++ src/main/scala/org/compevol/apg/hky/Site.scala
@@ -0,0 +1,19 @@
+package org.compevol.apg.hky
+
+import org.compevol.apg.{Nucleotide, Taxon}
+
+sealed trait Site
+
+case class TwoStateSite(red: Nucleotide, green: Nucleotide, allele: Taxon => Boolean) extends Site {
+
+  val substitution: Substitution = (red, green) match {
+    case (i, j) if (Set(i, j) subsetOf Nucleotide.stateSet) && i.state % 2 == j.state % 2 => Transition
+    case (i, j) if (Set(i, j) subsetOf Nucleotide.stateSet) && i.state % 2 != j.state % 2 => Transversion
+    case (Nucleotide.R, Nucleotide.Y) => Transversion
+    case (Nucleotide.Y, Nucleotide.R) => Transversion
+    case _ => throw new IllegalArgumentException
+  }
+
+}
+
+case class ConstantSite(nuc: Nucleotide) extends Site
