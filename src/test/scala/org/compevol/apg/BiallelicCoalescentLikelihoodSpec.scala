0bec01ed823302f8da62b4d30521e84a50fb5faa
Arman Bilge <armanbilge@gmail.com>
2016-03-17 13:55:03 +1300

    Correct text for test

--- src/test/scala/org/compevol/apg/BiallelicCoalescentLikelihoodSpec.scala
+++ src/test/scala/org/compevol/apg/BiallelicCoalescentLikelihoodSpec.scala
@@ -10,25 +10,25 @@ class BiallelicCoalescentLikelihoodSpec extends UnitSpec {
       val like = new BiallelicCoalescentLikelihood(2.0, 0.5, LinearSeq(CoalescentInterval(Double.PositiveInfinity, 1.0)))
       "samples = [(t = 0, p_red = [0.0, 0.0])]" should {
         val sample = LinearSeq(BiallelicSample(0, Seq(0.0, 0.0)))
-        "should be log(0.3)" in {
+        "be log(0.3)" in {
           assert(math.log(0.3) === like(sample))
         }
       }
       "samples = [(t = 0, p_red = [1.0, 0.0])]" should {
         val sample = LinearSeq(BiallelicSample(0, Seq(1.0, 0.0)))
-        "should be log(0.3)" in {
+        "be log(0.4)" in {
           assert(math.log(0.4) === like(sample))
         }
       }
       "samples = [(t = 0, p_red = [0.0, 1.0])]" should {
         val sample = LinearSeq(BiallelicSample(0, Seq(0.0, 1.0)))
-        "should be log(0.3)" in {
+        "be log(0.4)" in {
           assert(math.log(0.4) === like(sample))
         }
       }
       "samples = [(t = 0, p_red = [1.0, 1.0])]" should {
         val sample = LinearSeq(BiallelicSample(0, Seq(1.0, 1.0)))
-        "should be log(0.3)" in {
+        "be log(0.3)" in {
           assert(math.log(0.3) === like(sample))
         }
       }
