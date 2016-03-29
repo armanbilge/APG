8efe77ce4c0e0b325b1d0af8bbc7b8d405da14b9
Arman Bilge <armanbilge@gmail.com>
2016-03-29 14:45:33 +1000

    Finish likelihood implementation

--- src/main/scala/org/compevol/apg/hky/BiallelicLikelihood.scala
+++ src/main/scala/org/compevol/apg/hky/BiallelicLikelihood.scala
@@ -4,7 +4,7 @@ import org.compevol.apg._
 
 import scala.collection.LinearSeq
 
-class BiallelicLikelihood(val taxa: Set[Taxon], val mu: Double, val kappa: Double, val pi: IndexedSeq[Double], val coalescentIntervals: LinearSeq[CoalescentInterval], val errorProbs: Taxon => Double) extends (Map[Site, Double] => Double) {
+class BiallelicLikelihood(val taxa: Set[Taxon], val mu: Double, val kappa: Double, val pi: Map[Nucleotide, Double], val coalescentIntervals: LinearSeq[CoalescentInterval], val errorProbs: Taxon => Double) extends (Map[Site, Double] => Double) {
 
   override def apply(columns: Map[Site, Double]): Double = columns.view.par.map(Function.tupled({ (site, w) =>
 
@@ -15,15 +15,15 @@ class BiallelicLikelihood(val taxa: Set[Taxon], val mu: Double, val kappa: Doubl
       })
     })).to[LinearSeq].sorted
 
-    // TODO
-//    val L = (site match {
-//      case ConstantSite(n) => (Nucleotide.stateSet - n).view.map((_, n))
-//      case TwoStateSite(red, green, _) => Traversable((red, green))
-//    }).map(Function.tupled { (red, green) =>
-//      def freq(n: Nucleotide) = Nucleotide.unambiguous(n).map()
-//    })
+    val L = (site match {
+      case ConstantSite(n) => (Nucleotide.stateSet - n).view.map((_, n))
+      case TwoStateSite(red, green, _) => Traversable((red, green))
+    }).map(Function.tupled { (red, green) =>
+      val c = if (red.state % 2 == green.state % 2) 1.0 else kappa
+      new BiallelicCoalescentLikelihood(c * mu, pi(red) / (pi(red) + pi(green)), coalescentIntervals).apply(samples)
+    }).sum
 
-    0.0
+    L * w
 
   })).sum
 
