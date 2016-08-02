c88a5b7bd4854a2e5f760c142055203aaf38112a
Arman Bilge <armanbilge@gmail.com>
2016-08-02 18:04:23 +1200

    Fix text to match code

--- src/test/scala/org/compevol/apg/BiallelicCoalescentLikelihoodSpec.scala
+++ src/test/scala/org/compevol/apg/BiallelicCoalescentLikelihoodSpec.scala
@@ -6,21 +6,21 @@ class BiallelicCoalescentLikelihoodSpec extends UnitSpec {
 
   "BiallelicCoalescentLikelihood" when {
 
-    "mu = 2.0, pi = (0.5, 0.5), intervals = [(length = Inf, Ne = 1.0)]" when {
+    "mu = 2.0, pi = (0.5, 0.5), intervals = [(length = Inf, Ne = 2.0)]" when {
       val like = new BiallelicCoalescentLikelihood(2.0, 0.5, LinearSeq(CoalescentInterval(Double.PositiveInfinity, 2.0)))
-      "samples = [(t = 0, p_red = [0.0, 0.0])]" should {
+      "samples = [(t = 0, partial = [1, 0, 0])]" should {
         val sample = LinearSeq(TimePoint(0, IndexedSeq(1, 0, 0)))
         "be log(0.3)" in {
           assert(math.log(0.3) === like(sample))
         }
       }
-      "samples = [(t = 0, p_red = [1.0, 0.0])]" should {
+      "samples = [(t = 0, partial = [0, 1, 0])]" should {
         val sample = LinearSeq(TimePoint(0, IndexedSeq(0, 1, 0)))
         "be log(0.4)" in {
           assert(math.log(0.4) === like(sample))
         }
       }
-      "samples = [(t = 0, p_red = [1.0, 1.0])]" should {
+      "samples = [(t = 0, partial = [0, 0, 1])]" should {
         val sample = LinearSeq(TimePoint(0, IndexedSeq(0, 0, 1)))
         "be log(0.3)" in {
           assert(math.log(0.3) === like(sample))
