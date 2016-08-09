4a4817c4fffa425e290621209c4fa7372c866f7d
Arman Bilge <armanbilge@gmail.com>
2016-08-09 12:58:23 +1200

    Create TimePoint parser

--- src/main/scala/org/compevol/apg/TimePoint.scala
+++ src/main/scala/org/compevol/apg/TimePoint.scala
@@ -1,6 +1,17 @@
 package org.compevol.apg
 
+import java.io.File
+
+import scala.io.Source
+
 case class TimePoint(t: Double, redCountPartial: IndexedSeq[Double]) extends Ordered[TimePoint] {
   override def compare(that: TimePoint): Int = this.t compare that.t
   val k = redCountPartial.size - 1
+}
+
+object TimePoint {
+
+  def fromFile(t: Double, f: File): Array[TimePoint] =
+    Source.fromFile(f).getLines.map(l => TimePoint(t, l.split("\\s").map(_.toDouble).toIndexedSeq)).toArray
+
 }
