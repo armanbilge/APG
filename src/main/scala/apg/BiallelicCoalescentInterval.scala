a91710054e569b3fcab40e5051348caf60a678a0
Arman Bilge <armanbilge@gmail.com>
2017-01-24 22:40:39 -0800

    Correct handling of partials

--- src/main/scala/apg/BiallelicCoalescentInterval.scala
+++ src/main/scala/apg/BiallelicCoalescentInterval.scala
@@ -4,25 +4,28 @@ sealed trait BiallelicCoalescentInterval {
   val m: Int
   val k: Int
   val coalIndex: Int
+  val partial: Int => Double
+  def updatedUV(u: Double, v: Double): BiallelicCoalescentInterval
   def updatedCoalRate(coalRate: Double): BiallelicCoalescentInterval
 }
 
 object BiallelicCoalescentInterval {
 
-  def apply(length: Double, m: Int, k: Int, u: Double, v: Double, coalRate: Double, coalIndex: Int) = if (length.isInfinity)
-    InfiniteBiallelicCoalescentInterval(m, k, u, v, coalRate, coalIndex)
+  def apply(length: Double, m: Int, k: Int, u: Double, v: Double, coalRate: Double, coalIndex: Int, partial: Int => Double) = if (length.isInfinity)
+    InfiniteBiallelicCoalescentInterval(m, k, u, v, coalRate, coalIndex, partial)
   else
-    FiniteBiallelicCoalescentInterval(length, m, k, u, v, coalRate, coalIndex)
+    FiniteBiallelicCoalescentInterval(length, m, k, u, v, coalRate, coalIndex, partial)
 
 }
 
-case class FiniteBiallelicCoalescentInterval(length: Double, m: Int, k: Int, u: Double, v: Double, coalRate: Double, coalIndex: Int) extends BiallelicCoalescentInterval {
+case class FiniteBiallelicCoalescentInterval(length: Double, m: Int, k: Int, u: Double, v: Double, coalRate: Double, coalIndex: Int, partial: Int => Double) extends BiallelicCoalescentInterval {
 
+  def updatedUV(u: Double, v: Double): FiniteBiallelicCoalescentInterval = copy(u = u, v = v)
   def updatedCoalRate(coalRate: Double): FiniteBiallelicCoalescentInterval = copy(coalRate = coalRate)
 
 }
 
-case class InfiniteBiallelicCoalescentInterval(m: Int, k: Int, u: Double, v: Double, coalRate: Double, coalIndex: Int) extends BiallelicCoalescentInterval {
+case class InfiniteBiallelicCoalescentInterval(m: Int, k: Int, u: Double, v: Double, coalRate: Double, coalIndex: Int, partial: Int => Double) extends BiallelicCoalescentInterval {
 
   val pi = {
     import spire.std.array._
@@ -33,6 +36,7 @@ case class InfiniteBiallelicCoalescentInterval(m: Int, k: Int, u: Double, v: Dou
     pihat :/ z
   }
 
+  def updatedUV(u: Double, v: Double): InfiniteBiallelicCoalescentInterval = copy(u = u, v = v)
   def updatedCoalRate(coalRate: Double): InfiniteBiallelicCoalescentInterval = copy(coalRate = coalRate)
 
 }
--- src/main/scala/apg/BiallelicSiteLikelihood.scala
+++ src/main/scala/apg/BiallelicSiteLikelihood.scala
@@ -12,16 +12,14 @@ class BiallelicSiteLikelihood(val piRed: Double, val F: CachedF) extends Probabi
 
   lazy val evaluate = F.f.get(1, 0) * (1 - piRed) + F.f.get(1, 1) * piRed
 
+  def updatedUV(u: Double, v: Double): BiallelicSiteLikelihood = new BiallelicSiteLikelihood(piRed, F.updatedUV(u, v))
   def updatedCoalRate(i: Int, coalRate: Double): BiallelicSiteLikelihood = new BiallelicSiteLikelihood(piRed, F.updatedCoalRate(i, coalRate))
 
 }
 
 object BiallelicSiteLikelihood {
 
-  def createCachedF(intervals: LinearSeq[BiallelicCoalescentInterval], partials: LinearSeq[Int => Double]): CachedF = {
-    require(intervals.length == partials.length)
-    (intervals.tail, partials.tail).zipped.foldLeft[CachedF](new Base(intervals.head, partials.head))((cf, ip) => new Nested(ip._1, ip._2, cf))
-  }
+  def createCachedF(intervals: LinearSeq[BiallelicCoalescentInterval]): CachedF = intervals.tail.foldLeft[CachedF](new Base(intervals.head))((cf, int) => new Nested(int, cf))
 
   abstract class CachedF(val fc: (F, Double), val interval: BiallelicCoalescentInterval) {
     private[this] val (fp, cp) = fc
@@ -40,21 +38,24 @@ object BiallelicSiteLikelihood {
         for (n <- 1 to fp.getSize; r <- 0 to n) fp.set(n, r, fp.get(n, r) / cp)
         (MatrixExponentiator.expQTtx(finiteInterval.m, finiteInterval.u, finiteInterval.v, finiteInterval.coalRate, finiteInterval.length, fp), cp * cp)
     }
+    def updatedUV(u: Double, v: Double): CachedF
     def updatedCoalRate(i: Int, coalRate: Double): CachedF
   }
 
-  class Nested(interval: BiallelicCoalescentInterval, partial: Int => Double, previousF: CachedF) extends CachedF({
+  class Nested(interval: BiallelicCoalescentInterval, previousF: CachedF) extends CachedF({
     val F = new F(interval.m)
     for (i <- 0 to interval.k; n <- 1 to previousF.f.getSize; r <- 0 to n)
-      F.set(n + interval.k, r + i, F.get(n + interval.k, r + i) + math.max(previousF.f.get(n, r), 0) * partial(i) * HypergeometricPMF(n + interval.k, r + i, interval.k).apply(i))
+      F.set(n + interval.k, r + i, F.get(n + interval.k, r + i) + math.max(previousF.f.get(n, r), 0) * interval.partial(i) * HypergeometricPMF(n + interval.k, r + i, interval.k).apply(i))
     (F, previousF.c)
   }, interval) {
+
+    def updatedUV(u: Double, v: Double): Nested = new Nested(interval.updatedUV(u, v), previousF)
 
     def updatedCoalRate(i: Int, coalRate: Double): Nested = {
       val intervalp = if (interval.coalIndex == i) interval.updatedCoalRate(coalRate) else interval
       val previousFp = previousF.updatedCoalRate(i, coalRate)
       if ((interval ne interval) || (previousF ne previousFp))
-        new Nested(intervalp, partial: Int => Double, previousFp)
+        new Nested(intervalp, previousFp)
       else
         this
     }
@@ -63,12 +64,14 @@ object BiallelicSiteLikelihood {
 
   class Base(interval: BiallelicCoalescentInterval, f: F) extends CachedF((f, 1), interval) {
 
-    def this(interval: BiallelicCoalescentInterval, partial: Int => Double) = this(interval, {
+    def this(interval: BiallelicCoalescentInterval) = this(interval, {
       val F = new F(interval.m)
       for (r <- 0 to interval.m)
-        F.set(interval.m, r, partial(r))
+        F.set(interval.m, r, interval.partial(r))
       F
     })
+
+    def updatedUV(u: Double, v: Double): Base = new Base(interval.updatedUV(u, v), f)
 
     def updatedCoalRate(i: Int, coalRate: Double): Base = {
       if (interval.coalIndex == i)
