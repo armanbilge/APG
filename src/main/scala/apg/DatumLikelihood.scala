10cbc52c51b5071f4a0d85e55060401763fd7485
Arman Bilge <armanbilge@gmail.com>
2017-01-28 14:24:43 -0800

    Don't return negative values

--- src/main/scala/apg/DatumLikelihood.scala
+++ src/main/scala/apg/DatumLikelihood.scala
@@ -8,7 +8,7 @@ import spire.std.double._
 
 class DatumLikelihood[B, P <: Probability[Double], L <: Probability[Double]](val lit: Boolean @@ B, val probability: P, val lower: L) extends JointProbability[Double, P, L](probability, lower) {
 
-  override lazy val evaluate: Double = math.log(if (lit) probability.evaluate - lower.evaluate else lower.evaluate)
+  override lazy val evaluate: Double = math.log(if (lit) math.max(probability.evaluate - lower.evaluate, 0) else lower.evaluate)
 
   def flipped: DatumLikelihood[B, P, L] = new DatumLikelihood(tag[B](!lit), probability, lower)
 
