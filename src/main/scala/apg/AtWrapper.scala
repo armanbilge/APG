3d124de5694351780cb9959d71b8182963dec403
Arman Bilge <armanbilge@gmail.com>
2017-02-03 23:49:21 -0800

    Create AtWrapper

---
+++ src/main/scala/apg/AtWrapper.scala
@@ -0,0 +1,17 @@
+package apg
+
+import mcmc.Probability
+import monocle.Lens
+import monocle.function.At
+
+class AtWrapper[P <: Probability[Double]](val p: P, val i: Int) extends Probability[Double] {
+  override def evaluate: Double = p.evaluate
+}
+
+object AtWrapper {
+
+  implicit def atP[P <: Probability[Double], T](implicit p_t: Lens[P, T]): At[AtWrapper[P], Int, T] = { (i: Int) =>
+    Lens[AtWrapper[P], T](w => if (i == w.i) p_t.get(w.p) else null.asInstanceOf[T])(t => w => if (i == w.i) new AtWrapper(p_t.set(t)(w.p), i) else w)
+  }
+
+}
