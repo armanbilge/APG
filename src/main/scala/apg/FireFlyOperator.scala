9af1f21fa59966638c74c2ec44db81c29ff967bc
Arman Bilge <armanbilge@gmail.com>
2017-02-24 19:36:16 -0800

    Refactor operator for RDDs

--- src/main/scala/apg/FireFlyOperator.scala
+++ src/main/scala/apg/FireFlyOperator.scala
@@ -1,34 +1,39 @@
 package apg
 
 import mcmc.{Operator, Probability}
+import org.apache.spark.rdd.RDD
 import spire.random.Generator
+import spire.random.rng.MersenneTwister64
 
-import scala.collection.IterableLike
-import scala.collection.generic.CanBuildFrom
+class FireFlyOperator[B, P <: Probability[Double], L <: Probability[Double]](val `q_d->b`: Double) extends Operator[RDD[DatumLikelihood[B, P, L]], Double] {
 
-class FireFlyOperator[B, P <: Probability[Double], L <: Probability[Double], C <: IterableLike[DatumLikelihood[B, P, L], C]](val `q_d->b`: Double)(implicit rng: Generator, cbf: CanBuildFrom[C, DatumLikelihood[B, P, L], C]) extends Operator[C, Double] {
-
-  override def apply(c: C): C = c.map[DatumLikelihood[B, P, L], C] { dl =>
-    if (dl.lit) {
-      val u = rng.nextDouble()
-      val Ltilde = (dl.probability.evaluate - dl.lower.evaluate) / dl.lower.evaluate
-      if (`q_d->b` / Ltilde > u)
-        dl.flipped
-      else
-        dl
-    } else if (rng.nextDouble() < `q_d->b`) {
-      val u = rng.nextDouble()
-      val Ltilde = (dl.probability.evaluate - dl.lower.evaluate) / dl.lower.evaluate
-      if (Ltilde / `q_d->b` > u)
-        dl.flipped
-      else
-        dl
-    } else
-      dl
+  override def apply(c: RDD[DatumLikelihood[B, P, L]]): RDD[DatumLikelihood[B, P, L]] = {
+    val `q_d->b` = this.`q_d->b`
+    c.mapPartitionsWithIndex { (i, dls) =>
+      val rng = MersenneTwister64.fromTime()
+      dls.map { dl =>
+        if (dl.lit) {
+          val u = rng.nextDouble()
+          val Ltilde = (dl.probability.evaluate - dl.lower.evaluate) / dl.lower.evaluate
+          if (`q_d->b` / Ltilde > u)
+            dl.flipped
+          else
+            dl
+        } else if (rng.nextDouble() < `q_d->b`) {
+          val u = rng.nextDouble()
+          val Ltilde = (dl.probability.evaluate - dl.lower.evaluate) / dl.lower.evaluate
+          if (Ltilde / `q_d->b` > u)
+            dl.flipped
+          else
+            dl
+        } else
+          dl
+      }
+    }
   }
 
-  override def hastingsRatio(x: C, y: C): Double = (x, y).zipped.map { (x: DatumLikelihood[B, P, L], y: DatumLikelihood[B, P, L]) =>
+  override def hastingsRatio(x: RDD[DatumLikelihood[B, P, L]], y: RDD[DatumLikelihood[B, P, L]]): Double = x.zip(y).map(Function.tupled { (x: DatumLikelihood[B, P, L], y: DatumLikelihood[B, P, L]) =>
     x.evaluate - y.evaluate
-  }.sum
+  }).sum
 
 }
