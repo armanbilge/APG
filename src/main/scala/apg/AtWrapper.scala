1614fbe7d8406b16982a8176be9077ca73baf2a3
Arman Bilge <armanbilge@gmail.com>
2017-02-24 19:35:46 -0800

    Back to scala 2.11 syntax

--- src/main/scala/apg/AtWrapper.scala
+++ src/main/scala/apg/AtWrapper.scala
@@ -10,8 +10,10 @@ class AtWrapper[P <: Probability[Double]](val p: P, val i: Int) extends Probabil
 
 object AtWrapper {
 
-  implicit def atP[P <: Probability[Double], T](implicit p_t: Lens[P, T]): At[AtWrapper[P], Int, T] = { (i: Int) =>
-    Lens[AtWrapper[P], T](w => if (i == w.i) p_t.get(w.p) else null.asInstanceOf[T])(t => w => if (i == w.i) new AtWrapper(p_t.set(t)(w.p), i) else w)
+  implicit def atP[P <: Probability[Double], T](implicit p_t: Lens[P, T]): At[AtWrapper[P], Int, T] = new At[AtWrapper[P], Int, T] {
+    override def at(i: Int): Lens[AtWrapper[P], T] =
+      Lens[AtWrapper[P], T](w => if (i == w.i) p_t.get(w.p) else null.asInstanceOf[T])(t => w => if (i == w.i) new AtWrapper(p_t.set(t)(w.p), i) else w)
+
   }
 
 }
