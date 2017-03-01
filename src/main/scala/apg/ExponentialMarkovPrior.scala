4482e26fb5eac7d7ac3c46dd50ee1f1dbf95f8d6
Arman Bilge <armanbilge@gmail.com>
2017-02-28 20:20:30 -0800

    Replace 1/X prior with ExponentialMarkovPrior

---
+++ src/main/scala/apg/ExponentialMarkovPrior.scala
@@ -0,0 +1,33 @@
+package apg
+
+import mcmc.Probability
+import monocle.Lens
+import monocle.function.At
+import org.apache.commons.math3.distribution.GammaDistribution
+
+class ExponentialMarkovPrior[T <: Double, S <: Double, C <: Traversable[T]](val chain: C, val shape: S) extends Probability[Double] {
+
+  lazy val evaluate: Double = {
+    - math.log(chain.head) + chain.toIterator.sliding(2).withPartial(false).map { t =>
+      val mean = t.head
+      val x = t.tail.head
+      val scale = mean / shape
+      new GammaDistribution(shape, scale).logDensity(x)
+    }.sum
+  }
+
+}
+
+object ExponentialMarkovPrior {
+
+  def apply[T <: Double, S <: Double, C <: Traversable[T]](chain: C, shape: S) = new ExponentialMarkovPrior[T, S, C](chain, shape)
+
+  implicit def shape[T <: Double, S <: Double, C <: Traversable[T]]: Lens[ExponentialMarkovPrior[T, S, C], S] = Lens[ExponentialMarkovPrior[T, S, C], S](_.shape)(s => emp => new ExponentialMarkovPrior[T, S, C](emp.chain, s))
+
+  implicit def chain[T <: Double, S <: Double, C <: Traversable[T]]: Lens[ExponentialMarkovPrior[T, S, C], C] = Lens[ExponentialMarkovPrior[T, S, C], C](_.chain)(c => emp => new ExponentialMarkovPrior[T, S, C](c, emp.shape))
+
+  implicit def atChain[T <: Double, S <: Double, C <: Traversable[T], I](implicit atC: At[C, I, T]): At[ExponentialMarkovPrior[T, S, C], I, T] = new At[ExponentialMarkovPrior[T, S, C], I, T] {
+    override def at(i: I): Lens[ExponentialMarkovPrior[T, S, C], T] = chain ^|-> atC.at(i)
+  }
+
+}
