04430175b2eda570d6531a85885d6cf182896700
Arman Bilge <armanbilge@gmail.com>
2017-06-15 16:17:08 -0700

    Bump ammonite version

--- src/main/scala/apg/Main.scala
+++ src/main/scala/apg/Main.scala
@@ -7,12 +7,8 @@ import ammonite.util.Res.{Exception, Failure}
 
 object Main extends App {
 
-  ammonite.Main(predef = s"val args = ${args.tail.map(escape).mkString("Array(", ", ", ")")}", verboseOutput = true).runScript(Path(new File(args(0)).getAbsolutePath), Seq.empty, Seq.empty) match {
-    case Failure(ex, msg) =>
-      ex match {
-        case Some(t) => throw t
-        case None => throw new RuntimeException(msg)
-      }
+  ammonite.Main(predef = s"val args = ${args.tail.map(escape).mkString("Array(", ", ", ")")}", verboseOutput = true).runScript(Path(new File(args(0)).getAbsolutePath), Seq.empty)._1 match {
+    case Failure(msg) => throw new RuntimeException(msg)
     case Exception(t, msg) => throw t
     case _ =>
   }
