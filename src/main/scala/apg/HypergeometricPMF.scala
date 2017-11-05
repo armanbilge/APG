550f77f982c1c672a12f2611a83df4503d32b78c
Arman Bilge <armanbilge@gmail.com>
2017-11-05 14:24:57 -0800

    Synchronize hypergeometric

--- src/main/scala/apg/HypergeometricPMF.scala
+++ src/main/scala/apg/HypergeometricPMF.scala
@@ -6,14 +6,16 @@ import org.apache.commons.math3.exception.NotStrictlyPositiveException
 object HypergeometricPMF extends ((Int, Int, Int) => (Int => Double)) {
 
   def apply(N: Int, K: Int, n: Int): (Int => Double) = { k =>
-    if (N >= cache.length) {
-      cache = Array.tabulate(N + 1) { N =>
-        if (N < cache.length)
-          cache(N)
-        else
-          Array.tabulate(N+1, N+1) { (K, n) =>
-            Array.tabulate(n+1)(pmf(N, K, n, _))
-          }
+    HypergeometricPMF.this.synchronized {
+      if (N >= cache.length) {
+        cache = Array.tabulate(N + 1) { N =>
+          if (N < cache.length)
+            cache(N)
+          else
+            Array.tabulate(N+1, N+1) { (K, n) =>
+              Array.tabulate(n+1)(pmf(N, K, n, _))
+            }
+        }
       }
     }
     cache(N)(K)(n)(k)
