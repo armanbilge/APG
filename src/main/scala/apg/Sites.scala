76454aa2c80cc010866e29be7a0fd9cc40acdf42
Arman Bilge <armanbilge@gmail.com>
2017-11-05 14:26:37 -0800

    Change threshold to 0

--- src/main/scala/apg/Sites.scala
+++ src/main/scala/apg/Sites.scala
@@ -5,7 +5,7 @@ import scala.language.higherKinds
 
 object Sites {
 
-  def apply[D[X], B[X]](timePoints: Seq[Array[TimePoint]], threshold: Double = 1E-20)(implicit distributed: Distributed[D, B]): D[LinearSeq[TimePoint]] = {
+  def apply[D[X], B[X]](timePoints: Seq[Array[TimePoint]], threshold: Double = 0)(implicit distributed: Distributed[D, B]): D[LinearSeq[TimePoint]] = {
     import distributed._
     val broadcastedTimePoints = distributed.broadcast(timePoints)
     distributed.range(0, timePoints.head.length).map(_.toInt).map(i => distributed.retrieve(broadcastedTimePoints).map(_(i)).sorted.to[LinearSeq]).filter(_.forall(_.redCountPartial.exists(_ > threshold)))
