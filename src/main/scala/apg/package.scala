f0fd2fab8fe0a43e6f1a8539afe8f046a0707191
Arman Bilge <armanbilge@gmail.com>
2017-11-20 03:19:49 -0800

    Implement ArrayIsDistributed for testing

--- src/main/scala/apg/package.scala
+++ src/main/scala/apg/package.scala
@@ -9,7 +9,42 @@ package object apg {
   type F = FMatrix
   type Q = QMatrix
 
-  implicit object ArrayIsDistributed extends Distributed[ParArray, Some] {
+  implicit object ArrayIsDistributed extends Distributed[Array, Some] {
+
+    override def head[A](a: Array[A]): A = genericArrayOps(a).head
+
+    override def range(start: Int, end: Int): Array[Int] = (start until end).toArray
+
+    override def map[A, B : ClassTag](a: Array[A])(f: (A) => B): Array[B] = genericArrayOps(a).map(f)
+
+    override def filter[A](a: Array[A])(f: (A) => Boolean): Array[A] = genericArrayOps(a).filter(f)
+
+    override def partitionAwareMap[A, B : ClassTag, T](a: Array[A])(f: (Int) => T)(g: T => A => B): Array[B] = {
+      val t = f(0)
+      genericArrayOps(a).map(g(t))
+    }
+
+    override def zipMap[A, B : ClassTag, T : ClassTag](a1: Array[A], a2: Array[B])(f: (A, B) => T): Array[T] =
+      (a1, a2).zipped.map(f)
+
+    override def zipWithIndexMap[A, B : ClassTag](a: Array[A])(f: (A, Long) => B): Array[B] =
+      genericArrayOps(a).zipWithIndex.map(x => f(x._1, x._2))
+
+    def size[A](a: Array[A]): Long = a.length
+
+    override def sum(a: Array[Double]): Double = genericArrayOps(a).sum
+
+    override def persist[A](a: Array[A]): Array[A] = a
+
+    override def checkpoint[A](a: Array[A]): Unit = ()
+
+    override def broadcast[A : ClassTag](a: A): Some[A] = Some(a)
+
+    override def retrieve[A](s: Some[A]): A = s.value
+
+  }
+
+  implicit object ParArrayIsDistributed extends Distributed[ParArray, Some] {
 
     override def head[A](a: ParArray[A]): A = a.head
 
