7caa8fe6ce4b909a1d7d380e260fed801e867188
Arman Bilge <armanbilge@gmail.com>
2017-12-15 16:30:21 -0800

    Use PhantomReference instead of finalize()

--- src/main/scala/apg/NativePointer.scala
+++ src/main/scala/apg/NativePointer.scala
@@ -1,10 +1,27 @@
 package apg
 
 final class NativePointer(val value: Long) {
+  private[this] val ref = new NativePointer.Ref(this)
+}
 
-  override def finalize(): Unit = {
-    NativeLib.free(value)
-    super.finalize()
+object NativePointer {
+
+  import java.lang.ref.{PhantomReference, ReferenceQueue}
+
+  private[NativePointer] class Ref(ptr: NativePointer) extends PhantomReference[NativePointer](ptr, queue) {
+    val value = ptr.value
   }
+
+  private[this] val queue = new ReferenceQueue[NativePointer]
+
+  private[this] val thread = new Thread(() => while(true) {
+    val ref = queue.remove().asInstanceOf[Ref]
+    ref.clear()
+    NativeLib.free(ref.value)
+  })
+  thread.setName("native-gc")
+  thread.setDaemon(true)
+  thread.setUncaughtExceptionHandler((_, e) => e.printStackTrace())
+  thread.start()
 
 }
