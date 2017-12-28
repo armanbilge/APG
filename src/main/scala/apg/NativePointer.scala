cb7d9b7f365265782c62598f6a213c4d193f13cf
Arman Bilge <armanbilge@gmail.com>
2017-12-27 19:50:59 -0800

    Fix memory leak

--- src/main/scala/apg/NativePointer.scala
+++ src/main/scala/apg/NativePointer.scala
@@ -1,23 +1,50 @@
 package apg
 
+
+import java.util.concurrent.atomic.AtomicLong
+
+import scala.annotation.tailrec
+
 final class NativePointer(val value: Long) {
-  private[this] val ref = new NativePointer.Ref(this)
+  NativePointer.register(this)
 }
 
 object NativePointer {
 
-  import java.lang.ref.{PhantomReference, ReferenceQueue}
+  import scala.ref.{PhantomReference, ReferenceQueue}
+  import java.util.concurrent.atomic.AtomicReference
+
+  private[this] val queue = new ReferenceQueue[NativePointer]
+  private[this] val pointers = new AtomicReference(Set[Ref]())
+
+  def register(ptr: NativePointer): Unit = register(new Ref(ptr))
+
+  @tailrec
+  private[this] def register(ref: Ref): Unit = {
+    val current = pointers.get()
+    if (!pointers.compareAndSet(current, current + ref))
+      register(ref)
+  }
+
+  @tailrec
+  private[this] def unregister(ref: Ref): Unit = {
+    val current = pointers.get()
+    if (!pointers.compareAndSet(current, current - ref))
+      unregister(ref)
+  }
 
   private[NativePointer] class Ref(ptr: NativePointer) extends PhantomReference[NativePointer](ptr, queue) {
     val value = ptr.value
   }
 
-  private[this] val queue = new ReferenceQueue[NativePointer]
-
   private[this] val thread = new Thread(() => while(true) {
-    val ref = queue.remove().asInstanceOf[Ref]
-    ref.clear()
-    NativeLib.free(ref.value)
+    val ref = queue.remove match {
+      case Some(r: Ref) =>
+        unregister(r)
+        r.clear()
+        NativeLib.free(r.value)
+      case None =>
+    }
   })
   thread.setName("native-gc")
   thread.setDaemon(true)
