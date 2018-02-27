41737424c9f0fb0b30db3c8753720b7b16b2314d
Arman Bilge <armanbilge@gmail.com>
2018-02-27 22:52:53 +1300

    Bug fixes

--- scripts/merge_likes.py
+++ scripts/merge_likes.py
@@ -3,6 +3,7 @@
 import sys
 import functools as ft
 import itertools as it
+import numpy as np
 from numpy.polynomial.polynomial import polymul
 
 def print_join(iter):
@@ -19,9 +20,11 @@ fs = map(open, sys.argv[1:])
 z = None
 for l in zip(*fs):
     coeffs = [list(map(float, x.split())) for x in l]
-    if not z:
+    if z is None:
         z = manypolymul([1] * len(x) for x in coeffs)
     p = manypolymul(coeffs) / z
+    mp = np.max(p)
+    p /= mp
     pad = sum(map(len, coeffs)) - len(coeffs) - p.shape[0] + 1
     print_join(it.chain(map(str, p), it.repeat('0', pad)))
 
