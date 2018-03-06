2e1a519ddb0712b0ecafdba30f21993e58aca5ae
Arman Bilge <armanbilge@gmail.com>
2018-03-06 11:53:43 -0800

    Remove redundant cast

--- src/main/scala/apg/Sites.scala
+++ src/main/scala/apg/Sites.scala
@@ -8,7 +8,7 @@ object Sites {
   def apply[D[X], B[X]](timePoints: Seq[Array[TimePoint]], threshold: Double = 0)(implicit distributed: Distributed[D, B]): D[LinearSeq[TimePoint]] = {
     import distributed._
     val broadcastedTimePoints = distributed.broadcast(timePoints)
-    distributed.range(0, timePoints.head.length).map(_.toInt).map(i => distributed.retrieve(broadcastedTimePoints).map(_(i)).sorted.to[LinearSeq]).filter(_.forall(_.redCountPartial.exists(_ > threshold)))
+    distributed.range(0, timePoints.head.length).map(i => distributed.retrieve(broadcastedTimePoints).map(_(i)).sorted.to[LinearSeq]).filter(_.forall(_.redCountPartial.exists(_ > threshold)))
   }
 
 }
